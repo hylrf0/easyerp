cfab8ba6769adb8bf717c8fb367e1caa62106ca4
吹哔战将 <hylrf0@126.com>
2017-03-07 09:51:16 +0800

    Initial commit

---
+++ easyerp.web/src/main/java/org/easyerp/controller/IndexController.java
@@ -0,0 +1,16 @@
+package org.easyerp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Administrator on 2017/3/7 0007.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
